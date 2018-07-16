b43e4daf3ad6e2e96caca904b13e5fd5c93e9a12
Christian Schultz <bnlchris@web.de>
2018-07-16 21:51:59 +0200

    Has valid URL test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('has valid URL', function() {
+            allFeeds.forEach((feed) => {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+                expect(feed.url).toMatch('http');
+            });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
